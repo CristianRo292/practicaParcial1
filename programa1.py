8a12fb92489cfecccf4b774734372999eab3cd50
EQUIPO_001\roc53 <roc53629@gmail.com>
2025-08-25 13:32:02 -0600

    los primeros archivos

---
+++ programa1.py
@@ -0,0 +1,25 @@
+
+
+# a = 0
+
+# print("escribe un numero")
+
+a = int (input('escribe un numero: '))
+# print (a ** 2)
+print(a ** (1/2))
+
+# +
+# -
+# *
+# / (exacta con decimales)
+# // (sin decimales, lo trunca)
+# ** (elevar a potencia)  = 5**2 = 25
+# mod
+
+# and 
+# or 
+
+'''
+comentario de varias lineas
+'''
+
